e70c4993d65befbabf16f59f3d6e25d4223075c0
Jairo Nascimento <jaironsousa@gmail.com>
2025-09-02 14:45:54 -0300

    Visualização dos dados do banco

---
+++ src/main/java/br/com/jnsdev/codechella/TipoEvento.java
@@ -0,0 +1,13 @@
+package br.com.jnsdev.codechella;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 02/09/2025 - 14:39
+ */
+public enum TipoEvento {
+    SHOW,
+    CONCERTO,
+    TEATRO,
+    PALESTRA,
+    WORKSHOP;
+}
