ac4acbc28e144269fc95a05a2ba86ab4b29e9ea8
Jairo Nascimento <jaironsousa@gmail.com>
2025-09-02 11:16:40 -0300

    Criação api reativa closed issue #1

--- src/main/java/br/com/jnsdev/codechella/Application.java
+++ src/main/java/br/com/jnsdev/codechella/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodechellaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodechellaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
