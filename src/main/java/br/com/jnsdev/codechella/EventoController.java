d3c27da8e987e276e41123edd325cd5ceda0dd14
Jairo Nascimento <jaironsousa@gmail.com>
2025-09-07 07:29:36 -0300

    Implementando SSE no projeto. feat: #5

--- src/main/java/br/com/jnsdev/codechella/EventoController.java
+++ src/main/java/br/com/jnsdev/codechella/EventoController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 /**
  * @Autor Jairo Nascimento
  * @Created 02/09/2025 - 14:41
@@ -19,9 +21,15 @@ public class EventoController {
         this.service = service;
     }
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping
     public Flux<EventoDto> obterTodos() {
         return service.obterTodos();
+    }
+
+    @GetMapping(value = "/categoria/{tipo}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<EventoDto> obterPorTipo(@PathVariable String tipo) {
+        return service.obterPorTipo(tipo)
+                .delayElements(Duration.ofSeconds(4));
     }
 
     @GetMapping("/{id}")
--- src/main/java/br/com/jnsdev/codechella/EventoRepository.java
+++ src/main/java/br/com/jnsdev/codechella/EventoRepository.java
@@ -1,10 +1,12 @@
 package br.com.jnsdev.codechella;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 /**
  * @Autor Jairo Nascimento
  * @Created 02/09/2025 - 14:40
  */
 public interface EventoRepository extends ReactiveCrudRepository<Evento, Long> {
+    Flux<Evento> findByTipo(TipoEvento tipoEvento);
 }
